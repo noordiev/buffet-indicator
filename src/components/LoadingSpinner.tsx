280423b0f5e899ba47953e8040fca3ddc26390b0
noordiev <nurmukhammad.iev@gmail.com>
2024-12-08 12:38:28 +0300

    remove production files

--- src/components/LoadingSpinner.tsx
+++
@@ -1,7 +0,0 @@
-export function LoadingSpinner() {
-  return (
-    <div className="flex justify-center items-center h-[400px]">
-      <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600"></div>
-    </div>
-  );
-}
--- src/types/data.ts
+++
@@ -1,8 +0,0 @@
-export interface DataPoint {
-  date: string;
-  marketCap: number;
-  gdp: number;
-  ratio: number;
-}
-
-export type TimeRange = '1M' | '1Y' | '5Y' | '10Y' | 'ALL';
